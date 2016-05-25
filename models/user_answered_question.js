05f6c4132460079119490382ff7102df1ba62dc6
Vadim Voytenko <vadim.a.voytenko@gmail.com>
2016-05-25 19:36:22 +0300

    Quick commit.

--- models/user_answered_question.js
+++ models/user_answered_question.js
@@ -1,26 +1,18 @@
 
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('UserPassedTest', {
-
-        //primary key
-        idUserPassedTest: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
+    return sequelize.define('UserAnsweredQuestion', {
 
         //sql request
         answerRequest: {
             type: DataTypes.STRING(512),
             allowNull: false,
-            unique: false},
-        idUser: {
-            type: DataTypes.INTEGER,
-            unique: 'compositeIndex'
+            unique: false
         },
-        idTestHasQuestion: {
-            type: DataTypes.INTEGER,
-            unique: 'compositeIndex'
-        }
+
+        idUser : {type: DataTypes.INTEGER, primaryKey: true},
+
+        idTestHasQuestion : {type: DataTypes.INTEGER, primaryKey: true},
+
+        date: {primaryKey: true, type: DataTypes.DATE, allowNull: false, unique: false, defaultValue: sequelize.NOW}
     })
 };
